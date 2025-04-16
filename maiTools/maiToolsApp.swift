402d9bafef2ce5e2a390f709c8975e63bc4b6527
Raspberry <mashuo2855190178@icloud.com>
2025-04-16 09:25:39 +0800

    Initial Commit

---
+++ maiTools/maiToolsApp.swift
@@ -0,0 +1,17 @@
+//
+//  maiToolsApp.swift
+//  maiTools
+//
+//  Created by 马硕 on 2025/4/16.
+//
+
+import SwiftUI
+
+@main
+struct maiToolsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ maiToolsTests/maiToolsTests.swift
@@ -0,0 +1,16 @@
+//
+//  maiToolsTests.swift
+//  maiToolsTests
+//
+//  Created by 马硕 on 2025/4/16.
+//
+
+import Testing
+
+struct maiToolsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
